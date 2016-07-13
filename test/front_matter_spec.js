8e25b95188445149c9ef8f6e3d8621d78d7b7ab5
Sebastian Senf <contact@mustardamus.com>
2016-07-13 23:28:23 +0200

    front matter class, tests and fixtures

--- test/front_matter_spec.js
+++ test/front_matter_spec.js
@@ -3,12 +3,27 @@
 'use strict'
 
 const assert = require('assert')
+const path = require('path')
 const FrontMatter = require('../lib/front_matter')
 
 const frontMatter = new FrontMatter()
 
 describe('Front Matter Class', () => {
-  it('should do', () => {
-    assert.equal(frontMatter.works(), true)
+  it('should extract the front matter from a string', () => {
+    let matter = '---\n' +
+                 'boolean: true\n' +
+                 '---'
+    let obj = frontMatter.extractFromString(matter)
+
+    assert.equal(obj.boolean, true)
+  })
+
+  it('should extract the front matter from a file', () => {
+    let fixturePath = path.join(__dirname, 'fixtures/front_matter.txt')
+    let jsonPath = path.join(__dirname, 'fixtures/front_matter.json')
+    let obj = frontMatter.extractFromFile(fixturePath)
+    let json = require(jsonPath)
+
+    assert.deepEqual(obj, json)
   })
 })
