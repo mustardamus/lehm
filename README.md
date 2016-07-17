292ea5f2edee40ea9b5308f217d04be3b9a73d0a
Sebastian Senf <contact@mustardamus.com>
2016-07-17 14:36:21 +0200

    extended specs with description and ignore array, erased thoughts from readme

--- README.md
+++ README.md
@@ -1,26 +1,4 @@
-Wie könnte das aussehen? Einmal für nen gesamtes Projekt, und dann auch für
-Componenten.
+# Lehm
 
-## Project
-
-./templates/projects/vue-frontend = hier sind alle templates drin, und init file
-./templates/projects/vue-frontend/vue-frontend.js = diese init wird aufgerufen nachdem templates generiert und alle variablen gesammelt sind
-
-./templates/projects/vue-frontend/package.json = template:
-
-      {
-        "normal": true
-        [[if debug]]"debug: true"[[endif]]
-      }
-
-(project templates müssen keinen front-matter haben, dann werden sie einfach so
-geparsed und an den gleichen path kopiert)
-
-
-./templates/projects/vue-frontend/client/index.js = template:
-
-      ---
-      target: client/[[entryName]].js
-      ---
-
-      [[if debug]]console.log('debug')[[endif]]
+Work in progress. See the [Spec Fixture](fixtures/init-project/spec.txt) for a
+brief introduction.
--- test/fixtures/init-project/init-project.js
+++ test/fixtures/init-project/init-project.js
@@ -1,16 +1,30 @@
+// This .js file will not be treated as a template if it has the same name as
+// the parent directory.
+
+'use strict'
+
 module.exports = {
-  before: function (distPath, handlebars) {
+  description: 'A description what will be generated, requirements, etc',
+
+  // these folders/files will be ignored and not treated as templates
+  ignore: ['ignore', 'ignore.txt'],
+
+  before: function (distPath, vorpal, shell, handlebars) {
     // this function is called before the process starts
 
     // distPath - is the path where the files will be written
+    // vorpal - reference to the vorpal command instance (ie use vorpal.log)
+    // shell - a shell.js instance (ie execute system commands)
     // handlebars - is handlebars instance, useful for writing helpers
   },
 
-  after: function (distPath, variables) {
+  after: function (distPath, vorpal, shell, variables) {
     // this function is called after the process is completed and all files
     // have been generated. useful for running 'npm i' etc.
 
     // distPath - is the path where the files have been written
+    // vorpal - reference to the vorpal command instance (ie use vorpal.log)
+    // shell - a shell.js instance (ie execute system commands)
     // varialbles - an object of variables parsed from the templates and
     //              entered by the user via commandline
   }
