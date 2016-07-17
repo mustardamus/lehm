830d051a9fcc019ad8f40c5b6727d4751b181e40
Sebastian Senf <contact@mustardamus.com>
2016-07-17 15:25:51 +0200

    traverse templates and generate a simple object with description and files, fixtures and tests

---
+++ test/templates_spec.js
@@ -0,0 +1,25 @@
+/* global describe, it */
+
+'use strict'
+
+const assert = require('assert')
+const path = require('path')
+const fs = require('fs-extra')
+const Templates = require('../lib/templates')
+
+const templatesPath = path.join(__dirname, 'fixtures')
+const templates = new Templates(templatesPath)
+
+describe('Templates Class', () => {
+  it('should have set the templates path', () => {
+    assert.equal(templates.templatesPath, templatesPath)
+  })
+
+  it('should generate a object of all templates', () => {
+    let fixturePath = path.join(__dirname, 'fixtures/compare/templates.json')
+    let fixture = require(fixturePath)
+    let templatesObj = templates.getTemplates()
+
+    assert.deepEqual(templatesObj, fixture)
+  })
+})
