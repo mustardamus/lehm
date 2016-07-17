a00b35e0b37e99bf1ddebbe86e34718952a87e8c
Sebastian Senf <contact@mustardamus.com>
2016-07-17 19:59:03 +0200

    parse all files of a template for tags

--- test/parser_spec.js
+++ test/parser_spec.js
@@ -10,12 +10,25 @@ const Parser = require('../lib/parser')
 const parser = new Parser()
 
 describe('Parser Class', () => {
-  it('should get all variable names', () => {
+  it('should get all variable names from content', () => {
     let fixturePath = path.join(__dirname, 'fixtures/init-project/spec.txt')
     let jsonPath = path.join(__dirname, 'fixtures/compare/variables.json')
     let content = fs.readFileSync(fixturePath, 'utf8')
     let variables = parser.parseVariables(content)
     let json = require(jsonPath)
+
+    assert.deepEqual(variables, json)
+  })
+
+  it('should get all variable names from files', () => {
+    let jsonPath = path.join(__dirname, 'fixtures/compare/variables.json')
+    let json = require(jsonPath)
+    json.declaredInSub = { value: null, description: null }
+    json.sampleString.value = 'works'
+
+    let basePath = path.join(__dirname, 'fixtures/init-project')
+    let filesArr = ['spec.txt', 'sub/sub.txt']
+    let variables = parser.parseFiles(basePath, filesArr)
 
     assert.deepEqual(variables, json)
   })
