e10862b2f17f84eb17aba47be41778b4413a809f
Sebastian Senf <contact@mustardamus.com>
2016-07-23 14:20:11 +0200

    handlebars extending with custom delimiters rewrite in class, fixed tests

--- test/handlebars_spec.js
+++ test/handlebars_spec.js
@@ -3,20 +3,46 @@
 'use strict'
 
 const assert = require('assert')
-const HandlebarsFunc = require('../lib/handlebars')
+const Handlebars = require('../lib/handlebars')
 
 describe('Handlebars Proxy', () => {
+  it ('should set default delimiters if none passed', () => {
+    let handlebars = new Handlebars()
+    assert.equal(handlebars.delimiters, '{{ }}')
+  })
+
+  it ('should set custom delimiters if passed', () => {
+    let handlebars = new Handlebars('<% %>')
+    assert.equal(handlebars.delimiters, '<% %>')
+  })
+
   it('should use the default delimiters', () => {
-    let Handlebars = HandlebarsFunc()
-    let template = Handlebars.compile('{{ check }}')
+    let handlebars = new Handlebars()
+    let template = handlebars.compile('{{ check }}')
 
     assert.equal(template({ check: 'yes' }), 'yes')
   })
 
   it('should use the custom delimiters', () => {
-    let Handlebars = HandlebarsFunc('<% %>')
-    let template = Handlebars.compile('<% check %>')
+    let handlebars = new Handlebars('<% %>')
+    let template = handlebars.compile('<% check %>')
 
     assert.equal(template({ check: 'yes' }), 'yes')
+  })
+
+  it('should parse the template with default delimiters', () => {
+    let handlebars = new Handlebars()
+    let ast = handlebars.parse('{{ check }}').body
+
+    assert.equal(ast[0].type, 'MustacheStatement')
+    assert.equal(ast[0].path.original, 'check')
+  })
+
+  it('should parse the template with custom delimiters', () => {
+    let handlebars = new Handlebars('<% %>')
+    let ast = handlebars.parse('<% check %>').body
+
+    assert.equal(ast[0].type, 'MustacheStatement')
+    assert.equal(ast[0].path.original, 'check')
   })
 })
